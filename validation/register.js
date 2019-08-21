3ac11f5ac260b6dc86e3667bc164a5d93fee5d97
anh.vuong <anh.vuong@tbwa.com>
2019-08-21 14:02:08 +0700

    update validation register

--- validation/register.js
+++ validation/register.js
@@ -2,8 +2,8 @@ const Joi = require('joi')
 
 module.exports = function validateUser(user) {
   const schema = {
-    name: Joi.string().min(3).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
+    name: Joi.string().min(3).max(50).required(),
     password: Joi.string().min(3).max(255).required()
   }
 
